f1d9eef05ca7da954664aa1be5e4687bf242cc11
Chris Irag <irag.chrisandrei@mailbox.org>
2025-04-05 09:09:01 +0800

    Update navbar.svelte

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -15,13 +15,11 @@
 		<div class="advanced-search">
 			<span class="material-symbols-outlined">manage_search</span>
 		</div>
+	</div>
+	<div class="navcontainer right">
 		<div class="account">
 			<span class="material-symbols-outlined">account_circle</span>
 		</div>
-	</div>
-	<div class="navcontainer right">
-		<div class="menu-icon">
-			<span class="material-symbols-outlined">menu</span>
 	</div>
 </nav>
 
@@ -29,6 +27,14 @@
 	@media (prefers-color-scheme: dark) {
 		:root {
 			--icon-color: white;
+			--background-color: black;
+		}
+	}
+
+	@media (prefers-color-scheme: light) {
+		:root {
+			--icon-color: white;
+			--background-color: black;
 		}
 	}
 
@@ -51,18 +57,22 @@
 		left: 0; /* Align it to the left */
 		width: 100%; /* Make it span the full width */
 		display: flex;
-		justify-content: left;
+		flex-flow: row;
+		justify-content: space-between;
 		align-items: center;
-		background-color: #333;
+		background-color: var(--background-color);
 		padding: 10px 20px;
 		z-index: 1000; /* Ensure it stays above other elements */
 	}
 
-	.material-symbols-outlined {
-		font-variation-settings:
-			'FILL' 1,
-			'wght' 400,
-			'GRAD' 0,
-			'opsz' 24;
+	.navcontainer.left {
+		display: flex;
+		flex-flow: row;
+		gap: 10px;
+	}
+	.navcontainer.right {
+		display: flex;
+		flex-flow: row;
+		justify-content: left;
 	}
 </style>
