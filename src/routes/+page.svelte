992cfea837bf5836297b54ddc9ad955fec91bb67
iragca <irag.chrisandrei@mailbox.org>
2025-04-05 15:19:50 +0800

    removing margin of the body

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
-    import NavBar from '$lib/components/navbar.svelte';
+	import NavBar from '$lib/components/navbar.svelte';
+	import Dashboard from './dashboard.svelte';
 </script>
 
 <NavBar />
+<Dashboard />
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+</style>
