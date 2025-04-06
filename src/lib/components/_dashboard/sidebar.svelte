ff707a99d6ee89988814f0c8b6b1f0ba3ce1e60d
iragca <irag.chrisandrei@mailbox.org>
2025-04-06 15:58:11 +0800

    feat: sidebar component

---
+++ src/lib/components/_dashboard/sidebar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Profilecard from '$lib/components/_dashboard/_sidebar/profilecard.svelte';
+</script>
+
+<div class="background">
+	<div class="grid">
+		<Profilecard />
+		<slot name="content"></slot>
+		<slot name="footer"></slot>
+	</div>
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-rows: minmax(0, 100px) 1fr;
+	}
+
+	.background {
+		height: 100vh;
+		background-color: rgb(31, 31, 31, 0.5);
+        border-right: 2px solid var(--background-color-3);
+	}
+</style>
