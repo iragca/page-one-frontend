56c01499ff61f86ee806dcf83f91ef867d431c4f
iragca <irag.chrisandrei@mailbox.org>
2025-04-06 13:09:11 +0800

    feat: add error page and 500 error image

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import '../app.css';
+	import { page } from '$app/stores';
+</script>
+
+<div class="background">
+	<img class="error-image" src="/images/500.png" alt="500 Error" />
+    <p>{$page.status}: {$page.error?.message}</p>
+</div>
+
+<style>
+	p {
+		color: gray;
+		text-align: center;
+	}
+
+	.error-image {
+		width: 100%;
+		height: auto;
+		max-width: 300px;
+		margin: 0 auto;
+	}
+
+	.background {
+		display: flex;
+		flex-direction: column;
+		background-color: #171a1d;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
