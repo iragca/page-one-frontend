aab0ed2b1837cf27506859357985d3a97f9ce9ae
iragca <irag.chrisandrei@mailbox.org>
2025-04-06 15:23:19 +0800

    feat: add tooltip component with hover functionality

---
+++ src/lib/components/tooltip.svelte
@@ -0,0 +1,39 @@
+<script>
+	export let title = '';
+	let isHovered = false;
+	let x;
+	let y;
+	
+	function mouseOver(event) {
+		isHovered = true;
+		x = event.pageX + 20;
+		y = event.pageY + 20;
+	}
+	function mouseMove(event) {
+		x = event.pageX + 20;
+		y = event.pageY + 20;
+	}
+	function mouseLeave() {
+		isHovered = false;
+	}
+</script>
+
+<div
+	on:mouseover={mouseOver}
+  on:mouseleave={mouseLeave}
+	on:mousemove={mouseMove}>
+	<slot />
+</div>
+
+{#if isHovered}
+	<div style="top: {y}px; left: {x}px;" class="tooltip">{title}</div>
+{/if}
+
+<style>
+	.tooltip {
+		background: var(--dark-black);
+		border-radius: 4px;
+		padding: 4px;
+		position: absolute;
+	}
+</style>
