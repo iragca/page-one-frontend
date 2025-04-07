056b86c6f7ede71f171d25ba71dea113e61e6cf4
iragca <irag.chrisandrei@mailbox.org>
2025-04-07 19:21:30 +0800

    feat: implement book entry component and refactor gridview to use it

---
+++ src/lib/components/_dashboard/_bookbrowser/_books/book.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	let { title } = $props();
+
+	let hovering = $state(false);
+</script>
+
+<button class="book" onmouseenter={() => (hovering = true)} onmouseleave={() => (hovering = false)}>
+	{#if hovering}
+		<div class="layer book-icon"><span class="material-symbols-outlined"> menu_book </span></div>
+		<div class="layer vignette"></div>
+	{/if}
+	<img class="layer book-image" src="https://placehold.co/400x600" alt={title} />
+</button>
+
+<style>
+	.book {
+		all: unset;
+		display: grid;
+		place-items: center;
+		cursor: pointer;
+		height: 225px;
+		width: 150px;
+		border: 1px solid #ccc;
+		overflow: hidden;
+		object-fit: fill;
+	}
+
+    .layer {
+		grid-area: 1/1;
+	}
+
+	.book-icon {
+		display: flex;
+		background-color: var(--background-color);
+		align-items: center;
+		justify-content: center;
+		height: 40px;
+		width: 40px;
+		border-radius: 50%;
+		z-index: 99;
+	}
+
+    .material-symbols-outlined {
+        font-size: 20px;
+    }
+
+    .vignette {
+        width: 100%;
+        height: 100%;
+        background: radial-gradient(
+            ellipse at center,
+            rgba(0, 0, 0, 0) 10%,
+            rgba(0, 0, 0, 0.7) 100%
+        );
+        z-index: 2;
+    }
+
+	.book-image {
+		height: 100%;
+		width: 100%;
+		border: 1px solid #ccc;
+		overflow: hidden;
+		object-fit: cover;
+		z-index: 1;
+	}
+</style>
