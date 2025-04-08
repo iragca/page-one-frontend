6fc6fd13f435a963bdfa8c28004cb2cb3bd4dd5d
iragca <irag.chrisandrei@mailbox.org>
2025-04-08 12:53:22 +0800

    extracted components, made sort by button look better

--- src/lib/components/_dashboard/_bookbrowser/_options/sort-options.svelte
+++ src/lib/components/_dashboard/_bookbrowser/_options/sort-options.svelte
@@ -1,60 +1,32 @@
 <script>
 	import Dropdown from '$lib/components/dropdown/Dropdown.svelte';
-    import DropdownOption from '$lib/components/dropdown/DropdownOption.svelte';
+	import DropdownOption from '$lib/components/dropdown/DropdownOption.svelte';
+	import ButtonWithIcon from '$lib/components/buttons/ButtonWithIcon.svelte';
 
-    let showDropdown = $state(false);
-    let currentSort = $state('Title');
+	let showDropdown = $state(false);
+	let currentSort = $state('Title');
+
+	const toggleDropdown = () => {
+		showDropdown = !showDropdown;
+	};
 </script>
 
 <div class="background">
-    <div class="button-container">
-        <button class="sort button" onclick={() => (showDropdown = !showDropdown)}>
-            <span class="material-symbols-outlined"> sort </span>
-        </button>
-    </div>
-    {#if showDropdown}
-        <Dropdown>
-            <DropdownOption>Title</DropdownOption>
-            <DropdownOption>Author</DropdownOption>
-            <DropdownOption>Date</DropdownOption>
-            <DropdownOption>Genre</DropdownOption>
-        </Dropdown>
-    {/if}
+	<button class="button-container" onclick={toggleDropdown}>
+		<ButtonWithIcon />
+	</button>
+	{#if showDropdown}
+		<Dropdown>
+			<DropdownOption>Title</DropdownOption>
+			<DropdownOption>Author</DropdownOption>
+			<DropdownOption>Date</DropdownOption>
+			<DropdownOption>Genre</DropdownOption>
+		</Dropdown>
+	{/if}
 </div>
 
 <style>
 	.button-container {
-		border: 2px solid var(--dark-gray);
-		border-radius: 8px;
-
-		width: 40px;
-		height: 40px;
-
-		background-color: rgba(31, 31, 31, 0.4);
-		color: var(--light-gray);
-	}
-	.button-container:hover {
-		box-shadow: 0px 0px 8px var(--dark-black);
-		transition: 0.24s;
-		color: white;
-	}
-
-	.button-container:active {
-		box-shadow: 0px 0px 8px var(--dark-black);
-		transition: 0s;
-		border: 2px solid var(--light-gray);
-	}
-
-	.sort.button {
 		all: unset;
-		cursor: pointer;
-
-		display: flex;
-		justify-content: center;
-		align-items: center;
-
-		width: 100%;
-		height: 100%;
-		border-radius: 8px;
 	}
 </style>
---
+++ src/lib/components/buttons/ButtonWithIcon.svelte
@@ -0,0 +1,39 @@
+<div class="button-container">
+	<div class="sort button">
+		<span class="material-symbols-outlined"> sort </span>
+    </div>
+</div>
+
+<style>
+	.button-container {
+		border-radius: 8px;
+
+		width: 40px;
+		height: 40px;
+
+        color: white;
+	}
+	.button-container:hover {
+		box-shadow: 0px 0px 8px var(--dark-black);
+
+	}
+
+	.button-container:active {
+		box-shadow: 0px 0px 8px var(--dark-black);
+        transform: scale(0.90);
+		transition: 0s;
+	}
+
+	.sort.button {
+		all: unset;
+		cursor: pointer;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		width: 100%;
+		height: 100%;
+		border-radius: 8px;
+	}
+</style>
