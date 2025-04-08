2ea67a782770bf5d5997ec3b932fd38aecb14d9e
iragca <irag.chrisandrei@mailbox.org>
2025-04-08 17:13:26 +0800

    feat: add SimpleSearchBar component with styling

---
+++ src/lib/components/searchbars/SimpleSearchBar.svelte
@@ -0,0 +1,47 @@
+<div class="search-bar">
+	<input class="text-box" type="text" placeholder="Search..." />
+	<span class="material-symbols-outlined search">search</span>
+	<div class="icon">
+        <span class="material-symbols-outlined option">manage_search</span>
+
+	</div>
+</div>
+
+<style>
+	.search-bar {
+		background-color: rgba(255, 255, 255, 0.1);
+		border-radius: 8px;
+		padding-left: 8px;
+		padding-right: 8px;
+		align-items: center;
+		display: flex;
+		width: 50vw;
+        max-width: 400px;
+        height: 36px;
+        gap: 8px;
+	}
+
+	.text-box {
+        all: unset;
+		color: var(--dark-black);
+		border-radius: 8px;
+		outline: none;
+		width: 100%;
+		height: 100%;
+        text-align: left;
+        color: white;
+	}
+
+    .text-box::placeholder {
+        color: var(--light-gray);
+    }
+	.material-symbols-outlined.search {
+		color: var(--icon-color);
+        cursor: pointer;
+	}
+
+    .option {
+        color: var(--light-gray);
+        cursor: pointer;
+    }
+</style>
