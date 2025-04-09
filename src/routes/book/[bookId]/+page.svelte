17fc6f3c3eefaf715f34290f9af37d534592807a
iragca <irag.chrisandrei@mailbox.org>
2025-04-09 18:27:28 +0800

    feat: add BookInfoCard component and Book Details page

--- src/routes/book/[bookId]/+page.svelte
+++ src/routes/book/[bookId]/+page.svelte
@@ -2,6 +2,7 @@
 	import type { PageData } from './$types';
 
 	import BookNavBar from '$lib/components/bars/BookNavBar.svelte';
+	import BookInfoCard from '$lib/components/books/BookInfoCard.svelte';
 
 	let { data }: { data: PageData } = $props();
 
@@ -11,36 +12,45 @@
 <div class="background">
 	<div class="container">
 		<div class="header">
-            <BookNavBar {bookTitle} />
-        </div>
+			<BookNavBar {bookTitle} />
+		</div>
 		<div class="body">
-			<img src={book.cover_photo} alt={book.title + ' (image)'} class="book-cover" />
+			<BookInfoCard {book} />
 		</div>
 	</div>
 </div>
 
-
 {#snippet bookTitle()}
-{book.title}
-{/snippet}
-
-{#snippet bookCover()}
-    <img src={book.cover_photo} alt={book.title + ' (image)'} class="book-cover" />
+	{book.title}
 {/snippet}
 
 <style>
+	.background {
+		display: flex;
+		justify-content: center;
+		align-items: start;
+		height: 100vh;
+		width: 100%;
+	}
 	.container {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		border: 1px solid #ccc;
+		gap: 40px;
+		height: fit-content;
+		width: 800px;
+		margin-top: 4%;
 	}
 
-    .header {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-    }
+	.header {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+	}
 
+	.body {
+		max-height: fit-content;
+		max-width: 100%;
+	}
 </style>
