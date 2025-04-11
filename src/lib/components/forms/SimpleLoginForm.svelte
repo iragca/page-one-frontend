f31728e2a499ad382b81950fdf84f28c2504ce36
iragca <irag.chrisandrei@mailbox.org>
2025-04-11 21:55:30 +0800

    feat: refactor password input handling with visibility toggle and improved structure

--- src/lib/components/forms/SimpleLoginForm.svelte
+++ src/lib/components/forms/SimpleLoginForm.svelte
@@ -2,28 +2,49 @@
 	import SimpleInputWithIcon from './SimpleInputWithIcon.svelte';
 	import SimpleButtonWithText from '$lib/components/buttons/SimpleButtonWithText.svelte';
 	import SimpleLinkButton from '$lib/components/buttons/SimpleLinkButton.svelte';
-	import PasswordInputWithIcon from './PasswordInputWithIcon.svelte';
-    import LoginError from '../errors/LoginError.svelte';
+	import LoginError from '../errors/LoginError.svelte';
 
 	let hidePassword = $state(true);
+	let iconPass = $derived(hidePassword ? 'visibility' : 'visibility_off');
+	let inputType = $derived(hidePassword ? 'password' : 'text');
+	let toggle = () => {
+		hidePassword = !hidePassword;
+	};
 
-    let { form } = $props();
-
+	let { form } = $props();
 </script>
 
 <form class="background" method="POST">
 	<div class="top">
-        {#if form?.error}
-            <LoginError error_message={form.error} />
-        {/if}
-		<SimpleInputWithIcon icon="person" placeholder="Username" name="username" />
-		<PasswordInputWithIcon icon="key" placeholder="Password" visOff={hidePassword} name="password" />
+		{#if form?.error}
+			<LoginError error_message={form.error} />
+		{/if}
+		<SimpleInputWithIcon icon="" parentComponent={username} />
+		<SimpleInputWithIcon icon="" parentComponent={password} />
 	</div>
 	<div class="bottom">
 		<SimpleLinkButton icon="person_add" text="Signup" href="/signup" />
 		<SimpleButtonWithText icon="login" text="Login" type="submit" />
 	</div>
 </form>
+
+{#snippet username()}
+	<input type="text" name="username" placeholder="Username" class="text-box" required />
+{/snippet}
+
+{#snippet password()}
+	<input
+		type={inputType}
+		name="password"
+		placeholder="Password"
+		class="text-box"
+		required
+		minlength="8"
+	/>
+	<button type="button" class="visibility two" onclick={toggle}>
+		<span class="material-symbols-outlined"> {iconPass} </span>
+	</button>
+{/snippet}
 
 <style>
 	.background {
@@ -45,5 +66,27 @@
 		flex-direction: row;
 		gap: 8px;
 		width: 100%;
+	}
+
+	.text-box {
+		all: unset;
+		height: 36px;
+		width: 100%;
+	}
+
+	.text-box:user-invalid {
+		border: 1px solid var(--red);
+	}
+	.text-box::placeholder {
+		color: var(--light-gray);
+	}
+
+	.visibility {
+		all: unset;
+		display: flex;
+		align-self: left;
+		cursor: pointer;
+		color: var(--light-gray);
+		padding-right: 12px;
 	}
 </style>
