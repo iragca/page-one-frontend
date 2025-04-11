3e52ea38d9c8bd26c3ef90b43765552756fd6d13
iragca <irag.chrisandrei@mailbox.org>
2025-04-11 13:41:29 +0800

    feat: add signup page with SimpleSignUpForm and PasswordInputWithIcon components

---
+++ src/lib/components/forms/PasswordInputWithIcon.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	let { icon, placeholder, visOff } = $props();
+
+	let toggle = () => {
+		visOff = !visOff;
+	};
+
+	let input = $state('')
+
+</script>
+
+<div class="background">
+	<span class="material-symbols-outlined"> {icon} </span>
+
+	{#if visOff}
+		<input class="text-box" type="password" placeholder={placeholder} bind:value={input} />
+		<button class="visibility one" onclick={toggle}>
+			<span class="material-symbols-outlined"> visibility_off </span>
+		</button>
+	{:else}
+		<input class="text-box" type="text" placeholder={placeholder}  bind:value={input} />
+		<button class="visibility two" onclick={toggle}>
+			<span class="material-symbols-outlined"> visibility </span>
+		</button>
+	{/if}
+</div>
+
+<style>
+	.background {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: flex-start;
+		gap: 8px;
+
+		background-color: rgba(255, 255, 255, 0.1);
+		backdrop-filter: blur(12px); /* the blur gives the frosted effect */
+		-webkit-backdrop-filter: blur(12px); /* for Safari */
+		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+		padding-left: 8px;
+		padding-right: 12px;
+		color: var(--light-gray);
+	}
+
+	.background:focus-within {
+		background-color: rgba(255, 255, 255, 0.2);
+		box-shadow: 0px 0px 8px var(--dark-black);
+	}
+
+	.material-symbols-outlined {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.text-box {
+		all: unset;
+		height: 36px;
+		width: 100%;
+		color: white;
+	}
+	.text-box::placeholder {
+		color: var(--light-gray);
+	}
+
+	.visibility {
+		all: unset;
+		align-self: left;
+		cursor: pointer;
+		color: var(--light-gray);
+	}
+
+	.one:hover {
+		color: white;
+	}
+
+	.two {
+		color: white;
+	}
+</style>
---
+++ src/lib/components/forms/SimpleSignUpForm.svelte
@@ -0,0 +1,45 @@
+<script>
+	import SimpleInputWithIcon from './SimpleInputWithIcon.svelte';
+	import PasswordInputWithIcon from './PasswordInputWithIcon.svelte';
+	import SimpleButtonWithText from '$lib/components/buttons/SimpleButtonWithText.svelte';
+	import SimpleLinkButton from '$lib/components/buttons/SimpleLinkButton.svelte';
+
+	let hidePassword = $state(true);
+
+
+</script>
+
+<div class="background">
+	<div class="top">
+		<SimpleInputWithIcon icon="" placeholder="Username" />
+		<PasswordInputWithIcon icon="" placeholder="Password" visOff={hidePassword} />
+		<PasswordInputWithIcon icon="" placeholder="Confirm Password" visOff={hidePassword} />
+	</div>
+	<div class="bottom">
+		<SimpleLinkButton icon="arrow_back" text="" href="/login" />
+		<SimpleButtonWithText icon="person_add" text="Create Account" />
+	</div>
+</div>
+
+<style>
+	.background {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		height: 100%;
+	}
+
+	.top {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		width: 100%;
+	}
+
+	.bottom {
+		display: flex;
+		flex-direction: row;
+		gap: 8px;
+		width: 100%;
+	}
+</style>
