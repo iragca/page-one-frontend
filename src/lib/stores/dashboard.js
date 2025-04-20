2e0ef75c6d759e058828abe2b63ee777832a920c
iragca <irag.chrisandrei@mailbox.org>
2025-04-20 12:42:11 +0800

    added default book values

--- src/lib/stores/dashboard.js
+++ src/lib/stores/dashboard.js
@@ -3,7 +3,10 @@ import { writable } from 'svelte/store';
 export let viewMode = writable('grid');
 export let showBookDetails = writable(false);
 export let books = writable([]);
-export let chosenBook = writable(null);
+export let chosenBook = writable({
+    _id: null,
+    title: 'No title'
+});
 export let sortBy = writable('title');
 export let sortOrder = writable('asc');
 export let editMode = writable(false);
