0f3239c78a059c3e2ce27b7c6e1a01931c970a9c
iragca <irag.chrisandrei@mailbox.org>
2025-04-21 08:21:51 +0800

    renamed file, add removebook

--- src/lib/server/userbook.ts
+++ src/lib/server/userbook.ts
@@ -18,3 +18,22 @@ export const addbook = async (username: string, isbn_issn: string) => {
         throw new Error('Something went wrong.');
     }
 };
+
+export const removebook = async (username: string, isbn_issn: string) => {
+
+    const response = await fetch(`${BACKEND_API_URL}/books/user`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, isbn_issn })
+    });
+
+    if (response.ok) {
+        return await response.json();
+    } else if (response.status === 404) {
+        throw new Error('Book not found.');
+    } else {
+        throw new Error('Something went wrong.');
+    }
+}
